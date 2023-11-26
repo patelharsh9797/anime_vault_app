3b705ab3b265f2b6e4b2e106deb5aa411e0614a9
Harsh Patel <patelharsh9797@users.noreply.github.com>
2023-11-26 18:28:16 +0530

    fixed the delay getting too much thing

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -7,9 +7,11 @@ import { fetchAnime } from "@/app/action";
 
 let page = 2;
 
+type AnimeCardJSX = JSX.Element;
+
 function LoadMore() {
   const { ref, inView } = useInView();
-  const [data, setdata] = useState<AnimeProp[]>([]);
+  const [data, setdata] = useState<AnimeCardJSX[]>([]);
 
   useEffect(() => {
     if (inView) {
@@ -23,9 +25,7 @@ function LoadMore() {
   return (
     <>
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((item: AnimeProp, index: number) => (
-          <AnimeCard key={item.id} anime={item} index={index} />
-        ))}
+        {data}
       </section>
 
       <section className="flex justify-center items-center w-full">
