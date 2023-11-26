032e0fef19449cdfcf545f67393f85e8529b1e9f
Harsh Patel <patelharsh9797@users.noreply.github.com>
2023-11-26 19:29:17 +0530

    Added the types for detail and action update

--- app/action.tsx
+++ app/action.tsx
@@ -1,6 +1,8 @@
 "use server";
 
 import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+import { notFound } from "next/navigation";
+import { AnimeDetailsType } from "./types";
 
 export const fetchAnime = async (page: number = 1) => {
   const res = await fetch(
@@ -17,5 +19,20 @@ export const fetchAnimeDetails = async (id: string) => {
   const res = await fetch(`https://shikimori.one/api/animes/${id}`);
   const data = await res.json();
 
-  return data;
+  if (data.code === 404) {
+    return notFound();
+  }
+
+  return {
+    id: data.id,
+    name: data.name,
+    image: data.image,
+    description: data.description,
+    episodes: data.episodes,
+    released_on: data.released_on,
+    score: data.score,
+    screenshots: data.screenshots,
+    status: data.status,
+    url: data.url,
+  } as AnimeDetailsType;
 };
--- app/animes/[id]/page.tsx
+++ app/animes/[id]/page.tsx
@@ -1,8 +1,4 @@
 import { fetchAnimeDetails } from "@/app/action";
-import { notFound } from "next/navigation";
-import React from "react";
-
-// { params: { id: '30276' }, searchParams: {} }
 
 type AnimeDetailPageProps = {
   params: { id: string };
@@ -12,10 +8,6 @@ export default async function AnimeDetailPage({
   params: { id },
 }: AnimeDetailPageProps) {
   const data = await fetchAnimeDetails(id);
-
-  if (data.code === 404) {
-    return notFound();
-  }
 
   return (
     <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
